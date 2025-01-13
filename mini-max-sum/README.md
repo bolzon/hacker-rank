8d05d67a7679394499a0026afa2c689b3b8ea0ef
Alexandre Bolzon <blzn@mail.ru>
2025-01-13 15:44:59 +0100

    add mini max sum solution

---
+++ mini-max-sum/README.md
@@ -0,0 +1,5 @@
+# Mini-Max Sum
+
+https://www.hackerrank.com/challenges/mini-max-sum/problem
+
+Submitted in January 2025.
---
+++ mini-max-sum/mini_max_sum.py
@@ -0,0 +1,5 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+    arr = sorted(map(int, input().split()))
+    print(sum(arr[:-1]), sum(arr[1:]))
